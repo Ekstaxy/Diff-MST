ef2e03dd7d22f9257e8a727dbf0a37da932d9159
Your Name <you@example.com>
2025-12-04 17:04:41 +0800

    add .cpu()

--- mst/utils.py
+++ mst/utils.py
@@ -86,7 +86,7 @@ def run_diffmst(
         analysis_track = analysis_tracks[:, track_idx : track_idx + 1, :]
         track = tracks[:, track_idx : track_idx + 1, :]
         lufs_db = meter.integrated_loudness(
-            analysis_track.squeeze(0).permute(1, 0).numpy()
+            analysis_track.squeeze(0).permute(1, 0).cpu().numpy()
         )
         if lufs_db < -80.0:
             print(f"Skipping track {track_idx} due to low loudness {lufs_db}.")
