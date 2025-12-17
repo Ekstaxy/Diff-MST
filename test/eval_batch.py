167ce37009d2aee33260754c9c5c4688d6192099
Your Name <you@example.com>
2025-12-18 00:28:17 +0800

    fix text input

--- test/eval_batch.py
+++ test/eval_batch.py
@@ -71,7 +71,7 @@ def parse_args():
     # Evaluation parameters
     parser.add_argument("--num_songs", type=int, default=1, help='Number of songs to evaluate')
     parser.add_argument("--seed", type=int, default=42, help='Random seed for reproducibility')
-    parser.add_argument("--text_prompt", type=str, default="Bright", help='Text prompt to apply')
+    parser.add_argument("--text_prompt", type=str, nargs='+', default=["Bright"], help='Text prompt to apply')
     parser.add_argument("--interpolation", type=str, default="linear", help='Interpolation method: linear or slerp')
     parser.add_argument("--target_track_idx", type=int, default=1, help='Track index to apply text prompt to (0-based)')
     parser.add_argument("--output_dir", type=str, default="./eval_batch_outputs", help='Directory to save outputs')
