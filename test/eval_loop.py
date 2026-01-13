acaea9bf906951b6e81b535424317af4e92074b6
Your Name <you@example.com>
2026-01-13 20:12:42 +0800

    test text_info

--- test/eval_loop.py
+++ test/eval_loop.py
@@ -394,6 +394,7 @@ def main():
         # ITO
         text_info = args.control_info[c_idx]
         prompt_str = text_info[2]
+        print(text_info[0], type(text_info[0]))
         track_idx = int(text_info[0])
         bs, num_tracks, seq_len = tracks.size()
         print(f"[INFO] Using ITO text prompt: {prompt_str}")
